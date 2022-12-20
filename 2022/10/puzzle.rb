6e5a139b7cb2a4e29e5cc6f576a9cc736a80b61c
Chris <chris.norris-jones@clio.com>
2022-12-19 17:53:30 -0800

    Finish puzzle 10. Start 11

--- 2022/10/puzzle.rb
+++ 2022/10/puzzle.rb
@@ -10,6 +10,7 @@ class PuzzleSolver
   end
 
   def puzzle_2_answer
+    display_image
   end
 
   private def get_sum_of_signal_strengths
@@ -20,6 +21,16 @@ class PuzzleSolver
     end
 
     signal_detector.output_signal_strength
+  end
+
+  private def display_image
+    signal_detector = SignalStrengthDetector.new
+
+    instructions.each do |instruction|
+      signal_detector.handle_instruction(instruction)
+    end
+
+    signal_detector.output_display
   end
 
   private def instructions
@@ -37,11 +48,11 @@ class SignalStrengthDetector
     @cycles = 0
 
     @signal_sums = []
+
+    @screen = Array.new(6) { Array.new(40, ".") }
   end
 
   def handle_instruction(instruction)
-    puts "Instruction: #{instruction}. Current X: #{@x}. Current Cycles: #{@cycles}"
-
     if noop?(instruction)
       handle_noop
     elsif addx?(instruction)
@@ -54,13 +65,24 @@ class SignalStrengthDetector
     @signal_sums.slice(0, 6).sum
   end
 
+  def output_display
+    @screen.each do |line|
+      puts "#{line.join}"
+    end
+
+    nil
+  end
+
   private def noop?(instruction)
     instruction == "noop"
   end
 
   private def handle_noop
+    draw_pixel
     @cycles += 1
     check_if_signal_sum
+
+    nil
   end
 
   private def addx?(instruction)
@@ -70,22 +92,42 @@ class SignalStrengthDetector
   private def handle_addx(instruction)
     adder = instruction.split(" ").last.to_i
 
+    draw_pixel
     @cycles += 1
     check_if_signal_sum
+    draw_pixel
     @cycles += 1
     check_if_signal_sum
 
     @x += adder
+    nil
   end
 
   private def check_if_signal_sum
     if recordable_cycle?
-      puts "Cycle #{@cycles}. Adding #{@x} * #{@cycles} to Signal Sums."
       @signal_sums << @x * @cycles
+    end
+  end
+
+  private def draw_pixel
+    if pixel_position.include?(cycle_position)
+      @screen[current_screen_line][cycle_position] = "#"
     end
   end
 
   private def recordable_cycle?
     (@cycles + 20) % 40 == 0
+  end
+
+  private def current_screen_line
+    @cycles / 40
+  end
+
+  private def cycle_position
+    @cycles % 40
+  end
+
+  private def pixel_position
+    [@x - 1, @x, @x + 1]
   end
 end
